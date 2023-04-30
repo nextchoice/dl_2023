9430978fc68aed36f393dc7c542f18768229ab63
nextchoice <nextchoice@users.noreply.github.com>
2023-04-30 12:24:36 +0900

    Update settings.py
    deleting secret key

--- web/config/settings.py
+++ web/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#o_ppu5w$6ay-qw7156o@zo^b@qx0cc_=%l^j6tm%-vtv^^89u"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
